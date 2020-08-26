635e0ca8a1d60cb99b24dc6e2c02121dab123691
kimcaprio <kimcaprio1@nave.com>
2020-08-26 14:08:19 +0900

    commit all of bwce test project

---
+++ HttpSample/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/HttpSample/Tests/HttpSample1.bwt#/"/>
+  <EmulationDataFile>ACED000574000F4874747053616D706C65312E627774</EmulationDataFile>
+</emulation:BWTFile>
